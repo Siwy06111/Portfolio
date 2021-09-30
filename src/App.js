76ab259d387e73e188bde301eba15f068f5cc90e
Siwy061 <siwy061@o2.pl>
2021-09-30 12:31:39 +0200

    About Page Left Side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/Appasdafae.js</code> and asdasdasdassave to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactczzxczx
-        </a>
-      </header>
+    <div className="Wrapper">
+      <div className="About"> 
+        <div className="LeftSide"> 
+          <p>Hi! I'm Dawid, <br/>let me itroduce myself!</p>
+          <button className="AboutBtn">
+           <p> Tell me about You </p>
+           <i class="fas fa-arrow-down"></i>
+           </button>
+        </div>
+        <div className="RightSide">
+           cos tam
+        </div>
+      </div>
+
     </div>
   );
 }
